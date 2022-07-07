202753ee16a7cf15e56a9e5872746e782c514d94
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-07-07 22:53:07 +0430

    Update IERC721Metadata.sol

--- 07-IERC Lib/IERC721/IERC721Metadata.sol
+++ 07-IERC Lib/IERC721/IERC721Metadata.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8;
 /// @title ERC-721 Non-Fungible Token Standard, optional metadata extension
 /// @dev See https://eips.ethereum.org/EIPS/eip-721
 ///  Note: the ERC-165 identifier for this interface is 0x5b5e139f.
-interface ERC721Metadata /* is ERC721 */ {
+interface IERC721Metadata /* is ERC721 */ {
     /// @notice A descriptive name for a collection of NFTs in this contract
     function name() external view returns (string _name);
 
