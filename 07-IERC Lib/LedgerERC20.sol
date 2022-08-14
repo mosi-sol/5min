29ea0e6d310a9835cd70e4fac24e6d37000d284a
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-08-14 22:50:48 +0430

    Update LedgerERC20.sol

--- 07-IERC Lib/LedgerERC20.sol
+++ 07-IERC Lib/LedgerERC20.sol
@@ -3,21 +3,28 @@ pragma solidity 0.8;
 
 // ledger: basic version. not track asset`s by approved
 interface ILedger {
-    // mapping(uint256 => bytes) private _hashed;
+   function getResult(uint256 data) external view returns (uint256 id, address from, address to, uint256 amount, uint256 time);
+    function getData(uint256 data) external view returns (bytes memory);
 
-    event MintData(uint256 date);
-    
-    // function _hashedMint(uint256 amount, address to) internal view returns (bytes memory _crypto){
-    //     _crypto = bytes(abi.encode(amount, to, block.timestamp, msg.sender));
-    // }
-
-    function getData(uint256 date) external view returns (bytes memory);/* {
-        return _hashed[date];
-    }*/
-
-    function getResult(uint256 date) external view returns (uint256 amount, address to, uint256 time, address minter); /*{
-        bytes memory data = _hashed[date];
-        (amount, to, time, minter) = abi.decode(data, (uint256, address, uint256, address));
-    }*/
-
+    event MintData(uint256 id, uint256 date, bytes data);
 }
+
+/*
+uint256 private _ledgerId = 0;
+mapping(uint256 => bytes) private _hashed;
+
+function _compressData(uint256 amount, address to) internal returns (bytes memory _crypto){
+    _crypto = bytes(abi.encode(_ledgerId, msg.sender, to, amount, block.timestamp));
+    emit MintData(_ledgerId, block.timestamp, _crypto);
+    _ledgerId += 1;
+}
+
+function getData(uint256 data) external view virtual override returns (bytes memory) {
+    return _hashed[data];
+}
+
+function getResult(uint256 data) external view virtual override returns (uint256 id, address from, address to, uint256 amount, uint256 time) {
+    bytes memory info = _hashed[data];
+    (id, from, to, amount, time) = abi.decode(info, (uint256, address, address, uint256, uint256));
+}
+*/
